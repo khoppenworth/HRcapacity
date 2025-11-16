cef3b974e9a534a9ff29c8977b71ed736e039901
Kim Hoppenworth <khoppenworth@msh.org>
2025-11-15 19:30:00 -0500

    declare required PHP extensions

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hrassess/backend',
+        'pretty_version' => 'dev-work',
+        'version' => 'dev-work',
+        'reference' => 'e3313189780e4ce38cfb652b0706cd93c49613dd',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hrassess/backend' => array(
+            'pretty_version' => 'dev-work',
+            'version' => 'dev-work',
+            'reference' => 'e3313189780e4ce38cfb652b0706cd93c49613dd',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
